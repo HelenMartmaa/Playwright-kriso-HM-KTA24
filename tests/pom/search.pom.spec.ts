c3b88865b3943dd6c6f6cae7d4baed09aab68c43
Tatjana Kirotar <tatjana.kirotar@gmail.com>
2026-04-07 15:50:08 +0300

    refactored POM tests, added base page and updated tests in readme

--- tests/pom/search.pom.spec.ts
+++ tests/pom/search.pom.spec.ts
@@ -7,9 +7,30 @@
  *   - Use only: getByRole, getByText, getByPlaceholder, getByLabel
  */
 import { test, expect } from '@playwright/test';
+import type { Page } from '@playwright/test';
+import { HomePage } from '../../pages/HomePage';
+
+let page: Page;
+let homePage: HomePage;
 
 test.describe('Search for Books by Keywords (POM)', () => {
 
-  // TODO: implement tests
+  test.beforeAll(async ({ browser }) => {
+      const context = await browser.newContext();
+      page = await context.newPage();
+  
+      homePage = new HomePage(page);
+  
+      await homePage.openUrl();
+      await homePage.acceptCookies();
+    });
+  
+    test.afterAll(async () => {
+      await page.context().close();
+    });
+  
+    test('Test logo is visible', async () => {
+      await homePage.verifyLogo();
+    }); 
 
 });
